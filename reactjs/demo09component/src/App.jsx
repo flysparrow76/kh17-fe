f7b81425c522846b107cb35b8afd4bc8358040a3
flysparrow76 <tnqkr071376@gmail.com>
2026-07-02 10:57:43 +0900

    국가 등록 callback이용

--- reactjs/demo09component/src/App.jsx
+++ reactjs/demo09component/src/App.jsx
@@ -5,6 +5,7 @@ import Exam01 from "./components/Exam01"
 import Exam02 from "./components/Exam02"
 import Exam02_1 from "./components/Exam02_1"
 import Exam03 from "./components/Exam03"
+import Exam03_1 from "./components/Exam03_1"
 
 function App() {
 
@@ -16,8 +17,11 @@ function App() {
     <hr/>
     <Exam02/>
     <hr/>
-    <Exam02_1/> */}
+    <Exam02_1/>
+    <hr/>
     <Exam03/>
+    <hr/> */}
+    <Exam03_1/>
   </div>
   )
 }
