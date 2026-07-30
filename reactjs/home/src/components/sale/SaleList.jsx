fcf9f22fcab2dbc679a394cc964e0c0e6dbcb767
flysparrow76 <tnqkr071376@gmail.com>
2026-07-30 17:42:18 +0900

    상품 목록,상세페이지

---
+++ reactjs/home/src/components/sale/SaleList.jsx
@@ -0,0 +1,111 @@
+import Jumbotron from "@templates/Jumbotron";
+import { useCallback, useEffect, useState } from "react";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+
+import NoImage from "@assets/images/no-image.png";
+
+import { FaArrowRight, FaArrowTrendDown } from "react-icons/fa6";
+
+import { apiClient } from "@utils/reaxios";
+import { Link } from "react-router-dom";
+
+//무조건 상대경로로 불러올 때 ./ 부터 시작해야함 (파일명만 적으면 안됨)
+import "./SaleList.css";
+
+export default function SaleList() {
+    //data
+    const [items, setItems] = useState([]);
+    const loadItems = useCallback(async ()=>{
+        const { data } = await apiClient.post("/sale/list", {});
+        //data는 백엔드에서의 SaleListResponseVO
+        setItems(data.items);
+    }, []);
+    useEffect(()=>{
+        loadItems();
+    }, []);
+
+    //일회용 계산 함수
+    const calculateDiscountPercent = useCallback(({saleOriginalPrice, saleDiscountPrice})=>{
+        return 100 - (saleDiscountPrice * 100 / saleOriginalPrice);
+    }, []);
+
+    //view
+    return (<>
+        <Jumbotron title="상품 목록" content="원하는 상품을 클릭하여 상세 정보를 확인하세요!"/>
+        
+        {/* 상품 목록 - 카드 리스트 형태로 출력 */}
+        <Row className="mt-5">
+            <Col className="item-container">
+            {items.map(item=>{
+            
+            //추가 코드 작성 (현재 회차에서만 유효한 코드)
+            const { saleOriginalPrice, saleDiscountPrice } = item;
+            const percent = saleDiscountPrice * 100 / saleOriginalPrice;
+            const discount = 100 - percent;
+            const result = discount.toLocaleString();
+
+            const isDiscount = saleOriginalPrice > saleDiscountPrice;
+
+            const imageUrl = `${import.meta.env.VITE_SERVER_URL}/api/attach/${item.attachNo}`;
+
+            return (
+            <div key={item.saleNo} className="item mb-4 p-2">
+                <Card>
+                    <Card.Img variant="top" 
+                            src={item.attachNo === null ? NoImage : imageUrl} 
+                            style={
+                                {
+                                    width:"auto", 
+                                    height: 200, 
+                                    objectFit: "contain",
+                                    objectPosition: "center"
+                                }
+                            }/>
+                    <Card.Body>
+                        <Card.Title className="text-truncate">{item.saleName}</Card.Title>
+                        <Card.Text>
+                            <div>
+                                <Badge bg="info">{item.saleCategory}</Badge>
+                            </div>
+                            <div className="mt-4 fs-4" style={{height:120}}>
+                                {isDiscount ? (<>
+                                <s className="text-muted">{item.saleOriginalPrice.toLocaleString()} 원</s>
+                                <br/>
+                                <b className="text-danger">{item.saleDiscountPrice.toLocaleString()} 원</b>
+                                <br/>
+                                {/* ( ↓ {100 - item.saleDiscountPrice * 100 / item.saleOriginalPrice} % ) */}
+                                {/* ( ↓ {calculateDiscountPercent(item)} %) */}
+                                <span className="text-success">
+                                    <FaArrowTrendDown style={{transform:"rotate(55deg)"}}/> 
+                                    {result}%
+                                </span>
+                                </>) : (<>
+                                <b>{item.saleOriginalPrice.toLocaleString()} 원</b>
+                                </>) }
+                            </div>
+                        </Card.Text>
+
+                        <div className="text-end">
+                            <Button variant="primary" as={Link} to={`/sale/detail/${item.saleNo}`}>
+                                <span className="me-2">상세보기</span> 
+                                <FaArrowRight/>
+                            </Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            )})}
+            </Col>
+        </Row>
+    </>)
+}
+
+//내부적으로만 사용하는 하위 컴포넌트
+function ItemCard({item}) {
+
+    return (<></>)
+}
--- reactjs/home/src/templates/Body.jsx
+++ reactjs/home/src/templates/Body.jsx
@@ -33,6 +33,9 @@ import AdminUsersScroll from "@components/admin/AdminUsersScroll";
 import AdminUserDetail from "@components/admin/AdminUserDetail";
 import AdminSaleAdd from "@components/admin/sale/AdminSaleAdd";
 
+import SaleList from "@components/sale/SaleList";
+import SaleDetail from "@components/sale/SaleDetail";
+
 import TestMain from "@components/session/TestMain";
 
 import Private from "@guard/Private";
@@ -74,6 +77,11 @@ export default function Body() {
         <Route path="/admin/users2" element={<Admin><AdminUsersScroll/></Admin>}></Route>
         <Route path="/admin/detail/:accountId" element={<Admin><AdminUserDetail/></Admin>}></Route>
         <Route path="/admin/saleAdd" element={<Admin><AdminSaleAdd/></Admin>}></Route>
+
+        <Route path="/sale/list" element={<SaleList/>}></Route>
+        <Route path="/sale/detail/:saleNo" element={<SaleDetail/>}></Route>
+
+
 
         {/* 세션테스트 */}
         <Route path="/session/test" element={<TestMain/>}/>
