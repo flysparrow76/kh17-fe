ee68e9a27055e0f13feb4f402b7f3637a775bb3a
flysparrow76 <tnqkr071376@gmail.com>
2026-08-10 17:48:02 +0900

    카카오톡 스타일로 변경

--- reactjs/home/src/components/websocket/WebSocketV2AdvancedClient.jsx
+++ reactjs/home/src/components/websocket/WebSocketV2AdvancedClient.jsx
@@ -130,25 +130,33 @@ export default function WebSocketV2AdvancedClient(){
             <Col>
                 {/* 메세지 영역 생성 */}
                 <div className="message-wrapper">
-                    {history.map((message,index)=>(
-                        <div className="message-outer">
-                            <div className="message-inner">
-                                {/* 가로로 3칸을 나눠 순서대로 프로필/작성자+내영/작성시각으로 구현 */}
-                                <div className="profile-wrapper">
-                                    <img src="https://picsum.photos/100"/>
-                                </div>
-                                <div className="content-wrapper">
-                                    <div className="sender">피카츄</div>
-                                    <div className="content">
-                                        <div className="body">{message.content}</div>
-                                        <div className="time">
-                                            {dayjs(message.time).format("a h:mm")}
+                    {history.map((message,index)=>{
+                        //추가 계산 코드 작성
+                        const my = uuid ===message.sender;
+                        return(
+                            <div className= {`message-outer ${my ? "my" : ""}`}  key={index}>
+                                <div className="message-inner">
+                                    {/* 가로로 3칸을 나눠 순서대로 프로필/작성자+내영/작성시각으로 구현 */}
+                                    {my === false &&(
+                                        <div className="profile-wrapper">
+                                            <img src="https://picsum.photos/100"/>
+                                        </div>
+                                    )}
+                                    <div className="content-wrapper">
+                                        <div className="sender">피카츄</div>
+                                        <div className="content">
+                                            <div className="body">{message.content}</div>
+                                            <div className="time">
+                                                {dayjs(message.time).format("a h:mm")}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+
+                        )
+                    )}
+                    )}
                 </div>
             </Col>
         </Row>
